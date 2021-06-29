050367f48995e882c1907c8ab37e19a866b15ab3
Imszy17 <galihmardiana15@gmail.com>
2021-06-29 14:35:30 +0700

    gcast: change method gcast

--- DaisyXMusic/modules/gcast.py
+++ DaisyXMusic/modules/gcast.py
@@ -1,53 +1,58 @@
-# Daisyxmusic (Telegram bot project )
-# Copyright (C) 2021  Inukaasith
 
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
-
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
-
-import asyncio
-
-from pyrogram import Client
-from pyrogram import filters
-from pyrogram.types import Dialog
-from pyrogram.types import Chat
+from pyrogram import Client, filters
 from pyrogram.types import Message
-from pyrogram.errors import UserAlreadyParticipant
-
+from DaisyXMusic.leejieun import get_arg
+from DaisyXMusic.sql.chat_sql import load_chats_list, remove_chat_from_db
 from DaisyXMusic.services.callsmusic.callsmusic import client as USER
-from DaisyXMusic.config import SUDO_USERS
+from DaisyXMusic.helpers.filters import other_filters, other_filters2
+from DaisyXMusic.helpers.decorators import authorized_users_only, errors
+from io import BytesIO
 
-@Client.on_message(filters.command(["broadcast"]))
-async def broadcast(_, message: Message):
-    sent=0
-    failed=0
-    if message.from_user.id not in SUDO_USERS:
-        return
-    else:
-        wtf = await message.reply("`Starting a broadcast...`")
-        if not message.reply_to_message:
-            await wtf.edit("Please Reply to a Message to broadcast!")
-            return
-        lmao = message.reply_to_message.text
-        async for dialog in USER.iter_dialogs():
-            try:
-                await USER.send_message(dialog.chat.id, lmao)
-                sent = sent+1
-                await wtf.edit(f"`broadcasting...` \n\n**Sent to:** `{sent}` Chats \n**Failed in:** {failed} Chats")
-                await asyncio.sleep(3)
-            except:
-                failed=failed+1
-                #await wtf.edit(f"`broadcasting...` \n\n**Sent to:** `{sent}` Chats \n**Failed in:** {failed} Chats")
-                
-            
-        await message.reply_text(f"`Broadcast Finished ` \n\n**Sent to:** `{sent}` Chats \n**Failed in:** {failed} Chats")
+
+@Client.on_message(filters.command("gcast"))
+@authorized_users_only
+async def broadcast(client: Client, message: Message):
+    to_send = get_arg(message)
+    success = 0
+    failed = 0
+    for chat in load_chats_list():
+        try:
+            await client.send_message(
+                str(chat), 
+                to_send
+            )
+            success += 1
+        except:
+            failed += 1
+            pass
+    await message.reply(
+        f"Message sent to {success} chat(s). {failed} chat(s) failed recieve message"
+    )
+
+
+@Client.on_message(filters.command("chatlist"))
+async def chatlist(client, message):
+    chats = []
+    all_chats = load_chats_list()
+    for i in all_chats:
+        if str(i).startswith("-"):
+            chats.append(i)
+    chatfile = "Daftar chat.\n0. ID grup | Jumlah anggota | tautan undangan\n"
+    P = 1
+    for chat in chats:
+        try:
+            link = await client.export_chat_invite_link(int(chat))
+        except:
+            link = "Null"
+        try:
+            members = await client.get_chat_members_count(int(chat))
+        except:
+            members = "Null"
+        try:
+            chatfile += "{}. {} | {} | {}\n".format(P, chat, members, link)
+            P = P + 1
+        except:
+            pass
+    with BytesIO(str.encode(chatfile)) as output:
+        output.name = "chatlist.txt"
+        await message.reply_document(document=output, disable_notification=True)
